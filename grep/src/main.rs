e1f6b9934f1fd778dc22c3297c04a57228f4efab
lucasbaruj4 <barujalucas0@gmail.com>
2026-01-22 17:58:24 +0100

    grep works! it can find stuff inside a file and it can find stuff in the output of other commands (for now it only detects the ls command on linux hehe)

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
+use std::process::Command;
+use std::process::Output;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,39 +13,77 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Showing contents of {}", config.query);
-    println!("Inside of {}", config.file_path);
-    println!(" ");
+    if config.file_path != None {
+        println!("Showing matches of {:?}", config.query);
+        println!("Inside of {:?}", config.file_path.clone().unwrap());
+        println!(" ");
 
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
-        process::exit(1);
+        if let Err(e) = run(&config) {
+            println!("Application error: {}", e);
+            process::exit(1);
+        }
+    } else {
+        println!("Showing matches of {:?}", config.query);
+        println!("On the output of {:?}", config.command.clone().unwrap() + " " + &config.argument.clone().unwrap());
+        println!(" ");
+        let output = Command::new(Option::expect(config.command, "The command name isn't recognized")).arg(Option::expect(config.argument, "There weren't any arguments provided for the command")).output().expect("Failed to execute command");
+
+        if let Err(e) = grep_command_output(config.query, output) {
+            println!("Application error: {}", e);
+            process::exit(1);
+        }
+
     }
 }
 
 struct Config {
     query: String,
-    file_path: String,
+    file_path: Option<String>,
+    command: Option<String>,
+    argument: Option<String>
 }
 
 impl Config {
     fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
+        if args.len() < 2 {
             return Err("Not enough arguments, this program expects at least 2");
-        }
+        } else if args.len() == 4 {
+            let possible_command = match args[2].clone().as_str() {
+            "ls" => Some(String::from("ls")),
+            _ => None,
+            };
+
+            return Ok(Config{
+                query: args[1].clone(),
+                file_path: None,
+                command: possible_command,
+                argument: Some(args[3].clone())
+            })
+        }   
 
         Ok(Config {
             query: args[1].clone(),
-            file_path: args[2].clone(),
+            file_path: Some(args[2].clone()),
+            command: None,
+            argument: None
         })
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(Option::expect(config.file_path.clone(), "No filepath was provided."))?;
     for line in grep::search_case_insensitive(&config.query, &contents) {
         println!("{line}");
     }
     
+    Ok(())
+}
+
+fn grep_command_output(query: String, output: Output) -> Result<(), Box<dyn Error>> {
+    let contents = String::from_utf8(output.stdout).unwrap();
+    for line in grep::search_case_insensitive(&query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
